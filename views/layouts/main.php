7dd85b6624b8d2c9ee1e272a64cadb7a03e9c350
Nguyen The Thuc <thucfami@gmail.com>
2015-10-05 17:58:35 +0700

    menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-               // 'brandLabel' => Yii::t('app','My Company'),
+                'brandLabel' => 'Đầu trang',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
