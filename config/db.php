1c556059fa9ed4b0da9afc80bf122cdfa84c3e59
Nguyen The Thuc <thucfami@gmail.com>
2015-10-20 22:56:36 +0700

    config

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=toanhoc',
-    'username' => 'root',
-    'password' => '1',
-    'charset' => 'utf8',
-];
