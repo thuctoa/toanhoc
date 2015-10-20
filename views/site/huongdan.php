92b16416734e2832faf5eaed0e603c52dde10426
Nguyen The Thuc <thucfami@gmail.com>
2015-10-20 20:06:34 +0700

    pr website

--- views/site/huongdan.php
+++ views/site/huongdan.php
@@ -11,7 +11,7 @@ use yii\helpers\Url;
 $url= Url::to();
 if($url!='/matran/markov'){
 ?>
-<h3><b> Có một vài lưu ý của ứng dụng toán. </b></h3>
+<h3><b> Một vài lưu ý </b></h3>
 <?php
     }else{
 ?>
