d80b7a09367a7cc67b9e7dc8f10e9c8410dc12f6
Nguyen The Thuc <thucfami@gmail.com>
2015-10-31 21:38:33 +0700

    firefox chrome

--- views/site/test.php
+++ views/site/test.php
@@ -1,8 +1,21 @@
-<?php
-use yii\helpers\Html;
+<math xmlns="http://www.w3.org/1998/Math/MathML"> 
+    <mrow>
+       <mi>A</mi>
+       <mo>=</mo>
+       <mfenced open="(" close=")">
+          <mtable>
+             <mtr>
+                <mtd><mi>x</mi></mtd>
+                <mtd><mi>y</mi></mtd>
+                <mtd><mi>z</mi></mtd>
+             </mtr>
 
-/* @var $this yii\web\View */
-$this->title ="Ứng dụng toán học";
-?>
-<?= $this->helloWorld() ?>
-<?= $this->mumuoi('haha') ?>
+             <mtr>
+                <mtd><mi>z</mi></mtd>
+                <mtd><mi>w</mi></mtd>
+                <mtd><mi>z</mi></mtd>
+             </mtr>
+          </mtable>
+       </mfenced>
+    </mrow>
+</math>
