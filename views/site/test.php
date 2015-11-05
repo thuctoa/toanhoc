4e55640b5f951d9e5bc72ae703a3dceb3df39e5f
Nguyen The Thuc <thucfami@gmail.com>
2015-11-05 22:49:07 +0700

    test html

--- views/site/test.php
+++ views/site/test.php
@@ -1,21 +1,12 @@
-<math xmlns="http://www.w3.org/1998/Math/MathML"> 
-    <mrow>
-       <mi>A</mi>
-       <mo>=</mo>
-       <mfenced open="(" close=")">
-          <mtable>
-             <mtr>
-                <mtd><mi>x</mi></mtd>
-                <mtd><mi>y</mi></mtd>
-                <mtd><mi>z</mi></mtd>
-             </mtr>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
 
-             <mtr>
-                <mtd><mi>z</mi></mtd>
-                <mtd><mi>w</mi></mtd>
-                <mtd><mi>z</mi></mtd>
-             </mtr>
-          </mtable>
-       </mfenced>
-    </mrow>
-</math>
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
