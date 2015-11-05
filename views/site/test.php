72f02730da98c1357460e4ebbd287de9c2334d73
Nguyen The Thuc <thucfami@gmail.com>
2015-11-05 23:01:15 +0700

    test html

--- views/site/test.php
+++ views/site/test.php
@@ -9,6 +9,24 @@
 <h2>Ứng dụng toán</h2>
 <h3>Ứng dụng toán</h3>
 <p>Ứng dụng toán.</p>
-
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+  <url>
+    <loc>http://www.example.org/business/article55.html</loc>
+    <news:news>
+      <news:publication>
+        <news:name>Ứng dụng toán</news:name>
+        <news:language>vi</news:language>
+      </news:publication>
+      <news:access>Ứng dụng toán</news:access>
+      <news:genres>Ứng dụng toán</news:genres>
+      <news:publication_date>2025-11-05</news:publication_date>
+      <news:title>Ứng dụng toán</news:title>
+      <news:keywords>Ứng dụng toán</news:keywords>
+      <news:stock_tickers>Ứng dụng toán</news:stock_tickers>
+    </news:news>
+  </url>
+</urlset>
 </body>
 </html>
